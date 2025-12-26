2952dcf9e090bfa08269eef4d2056d65c549007d
shadrach68 <ugwokeshadrach68@gmail.com>
2025-12-26 23:12:56 +0100

    work on the navbar

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,12 @@
 
 const close = document.getElementById("open");
-const open = document.getElementById("close").addEventListener("click",()=>{
+const open = document.getElementById("close")
+const line1 = document.getElementById("line1")
+const line2 = document.getElementById("line2")
+
+open.addEventListener("click",()=>{
     close.classList.toggle("openn")
+    line1.classList.toggle("line1")
+    line2.classList.toggle("line2")
+    // console.log(close.classList);
 })
